04183883c6df3467f8871ac3e9805c749cba776a
Ric Clark <rclark@eimedical.com>
2025-06-17 21:31:27 -0600

    Changed database table to cow_records

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { renderHtml } from "./renderHtml";
 
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+    const stmt = env.DB.prepare("SELECT * FROM cow_records LIMIT 3");
     const { results } = await stmt.all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
