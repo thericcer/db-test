c20691b1233c35e0f989f1ce13aa2efd49935133
Ric Clark <rclark@eimedical.com>
2025-06-17 21:46:48 -0600

    Added cow_records database table.

---
+++ migrations/0002_create_cow_records.sql
@@ -0,0 +1,14 @@
+-- Migration number: 0001 	 2024-12-27T22:04:18.794Z
+CREATE TABLE IF NOT EXISTS cow_records (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tag_id TEXT NOT NULL,
+    insemination_date TEXT,
+    exam_date TEXT
+);
+
+-- Insert some sample data into our comments table.
+INSERT INTO cow_records (author, content)
+VALUES
+    ('112233445566778899', '22-04-2025'),
+    ('deadbeefcofeecofee', '22-04-2025', '05-05-2025')
+;
